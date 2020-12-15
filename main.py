ae13369b1e66d4c4fa25aa2220487d2591297ab4
Tim <timkettenacker@Tims-MacBook-Pro.local>
2020-12-15 11:06:09 +0100

    created a fastapi skeleton

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# if you want to work with virtualenv, you need to initially follow the steps outlined in
+# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+# to create a virtualenv, navigate to the project directory and put "python3 -m venv env" in cmd,
+# effectively creating a folder "env" containing the environment that can be activated using
+# "source env/bin/activate", you can "pip install" packages into that. "pip freeze > requirements.txt"
+# creates and stores a list of all packages and their dependencies into the project folder.
+# "uvicorn main:app" starts up the server, specifically looking for file named "main" and object "app"
+# in it. It can be reached under "http://127.0.0.1:8000" and its OpenAPI description under "/docs".
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+# the function right below is in charge of of handling requests that go to path "/" using a GET
+def root():
+    return {"message": "Hello World"}
